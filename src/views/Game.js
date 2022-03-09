2fc871d8d335670154b0f7190e6fd55fb5db52dd
Marina <marina.robleno@invyo.io>
2022-03-09 17:13:52 +0100

    added array

--- src/views/Game.js
+++ src/views/Game.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BigContainer } from "../components/BigContainer";
 import { Input } from "../components/Input";
 import { PlayAgain } from "../components/PlayAgain";
 
 export function Game() {
   const [submittedWord, setSubmittedWord] = useState(null);
-  const [correctWord, setCorrectWord] = useState("verga");
+  const [correctWord, setCorrectWord] = useState("");
   const [solved, setSolved] = useState(false);
+  const arrayPalabras = ["amigo", "comer", "puedo"];
+
+  useEffect(() => {
+    setCorrectWord(
+      arrayPalabras[Math.floor(Math.random() * arrayPalabras.length)]
+    );
+  }, []);
   return (
     <>
       <h1>PALABREN</h1>
